6ab2ff8302714d9c506f9390d1907ee763572aee
Mikko Pyykkönen <pyykkonenmikko@gmail.com>
2024-06-06 15:15:35 +0300

    osa3 jatkoa

--- osa3/nodebackend/index.js
+++ osa3/nodebackend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
 
 let notes = [
   {
@@ -20,7 +21,6 @@ let notes = [
   }
 ]
 
-
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
@@ -28,8 +28,55 @@ app.get('/', (request, response) => {
   app.get('/api/notes', (request, response) => {
     response.json(notes)
   })
+
+  app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    
   
-  const PORT = 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
   })
+
+  app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+  
+    response.status(204).end()
+  })
+
+  
+
+  const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+  
+  app.post('/api/notes', (request, response) => {
+    const body = request.body
+  
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const note = {
+      content: body.content,
+      important: body.important || false,
+      id: generateId(),
+    }
+  
+    notes = notes.concat(note)
+  
+    response.json(note)
+  })
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)

5651a4623d560f7cff869dea811461ebdc4dba3c
Mikko Pyykkönen <pyykkonenmikko@gmail.com>
2024-11-29 13:35:17 +0200

    Tehtäviä

--- osa8/library-back/models/user.js
+++ osa8/library-back/models/user.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const schema = new mongoose.Schema({
   username: {
@@ -11,13 +11,13 @@ const schema = new mongoose.Schema({
   friends: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Person',
-    },
+      ref: 'Person'
+    }
   ],
   favoriteGenre: {
-    type: String, // Vaihda ObjectId merkkijonoksi
+    type: String,
   },
-});
+})
 
-schema.plugin(uniqueValidator);
-module.exports = mongoose.model('User', schema);
+schema.plugin(uniqueValidator)
+module.exports = mongoose.model('User', schema)
